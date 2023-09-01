40d771cd2f7ef6fc5da4b94eb8793ea45fa8eecb
Mateo Loaiza <141420478+Matthub05@users.noreply.github.com>
2023-09-01 07:25:52 -0500

    Se creo el protecto

---
+++ Mascotas/src/mascotas/Mascotas.java
@@ -0,0 +1,16 @@
+package mascotas;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Mascotas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
